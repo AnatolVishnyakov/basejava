7c1f33bfefe86ea5eb047a54fb33932a9fe6d0a6
Anatol V <malfenius@gmail.com>
2019-01-21 21:05:35 +0300

    HW-2. Changes for review. Delete javadoc's

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -18,9 +18,6 @@ public class ArrayStorage {
         }
     }
 
-    /**
-     * @param resume Resume
-     */
     public void save(Resume resume) {
         if (size >= DEFAULT_CAPACITY) {
             System.out.println("Array index out of bounds.");
@@ -34,10 +31,6 @@ public class ArrayStorage {
         }
     }
 
-    /**
-     * @param uuid String
-     * @return Resume
-     */
     public Resume get(String uuid) {
         int index = indexOf(uuid);
         if (index == RESUME_NOT_FOUND) {
@@ -48,9 +41,6 @@ public class ArrayStorage {
         return storage[index];
     }
 
-    /**
-     * @param uuid String
-     */
     public void delete(String uuid) {
         int index = indexOf(uuid);
         if (index == RESUME_NOT_FOUND) {
@@ -61,9 +51,6 @@ public class ArrayStorage {
         }
     }
 
-    /**
-     * @param resume Resume
-     */
     public void update(Resume resume) {
         int index = indexOf(resume.getUuid());
         if (index == RESUME_NOT_FOUND) {
@@ -73,9 +60,6 @@ public class ArrayStorage {
         }
     }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
     public Resume[] getAll() {
         return Arrays.copyOf(storage, size);
     }
