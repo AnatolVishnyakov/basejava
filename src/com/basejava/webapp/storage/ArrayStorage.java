0a1c0e5a1dc06cdc4c197da900de5bfed2c9bb66
Anatol V <malfenius@gmail.com>
2019-01-28 22:52:12 +0300

    HW-3. Refactoring. Create abstract class. Implement template methods

---
+++ src/com/basejava/webapp/storage/ArrayStorage.java
@@ -0,0 +1,24 @@
+package com.basejava.webapp.storage;
+
+import com.basejava.webapp.model.Resume;
+
+/**
+ * Array based storage for Resumes
+ */
+public class ArrayStorage extends AbstractArrayStorage {
+
+    @Override
+    protected int indexOf(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return i;
+            }
+        }
+        return RESUME_NOT_FOUND;
+    }
+
+    @Override
+    protected void insertResume(Resume resume) {
+        storage[size++] = resume;
+    }
+}
