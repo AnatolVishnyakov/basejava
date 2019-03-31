f7dec0a70ee0e31d95f04ff4bbdc400c89db605b
Anatol V <malfenius@gmail.com>
2019-03-31 22:19:55 +0300

    HW-6. Fixed structured unit test.

--- test/com/basejava/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/basejava/webapp/storage/AbstractArrayStorageTest.java
@@ -1,7 +1,24 @@
 package com.basejava.webapp.storage;
 
+import com.basejava.webapp.exception.StorageException;
+import com.basejava.webapp.model.Resume;
+import org.junit.Assert;
+import org.junit.Test;
+
 public abstract class AbstractArrayStorageTest extends AbstractStorageTest {
     protected AbstractArrayStorageTest(Storage storage) {
         super(storage);
+    }
+
+    @Test(expected = StorageException.class)
+    public void saveOverflow() {
+        try {
+            for (int i = 4; i <= AbstractArrayStorage.DEFAULT_CAPACITY; i++) {
+                storage.save(new Resume(String.format("uuid-%s", i)));
+            }
+        } catch (StorageException e) {
+            Assert.fail();
+        }
+        storage.save(new Resume());
     }
 }
