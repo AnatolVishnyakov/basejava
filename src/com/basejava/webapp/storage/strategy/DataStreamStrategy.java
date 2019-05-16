60df7267771928f6a88517645b112069d7404b80
Anatol V <malfenius@gmail.com>
2019-05-16 22:23:42 +0300

    HW-10. Add new strategy for works with DataStreamInput/DataStreamOutput.

---
+++ src/com/basejava/webapp/storage/strategy/DataStreamStrategy.java
@@ -0,0 +1,18 @@
+package com.basejava.webapp.storage.strategy;
+
+import com.basejava.webapp.model.Resume;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class DataStreamStrategy implements AbstractStrategy {
+    @Override
+    public void writeResume(Resume resume, OutputStream outputStream) throws IOException {
+    }
+
+    @Override
+    public Resume readResume(InputStream inputStream) throws IOException {
+        return null;
+    }
+}
