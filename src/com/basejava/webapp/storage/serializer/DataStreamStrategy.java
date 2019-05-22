c014b91a145bd007fc0cc56026ee9da72d567926
Anatol V <malfenius@gmail.com>
2019-05-22 22:47:30 +0300

    HW-10. Fixed DataStreamStrategy by review. Changed write elements collection.

--- src/com/basejava/webapp/storage/serializer/DataStreamStrategy.java
+++ src/com/basejava/webapp/storage/serializer/DataStreamStrategy.java
@@ -5,8 +5,13 @@ import com.basejava.webapp.model.*;
 import java.io.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
-import java.util.Map;
+
+@FunctionalInterface
+interface DataStreamWriter<T> {
+    void write(T t) throws IOException;
+}
 
 public class DataStreamStrategy implements AbstractStrategy {
     @Override
@@ -15,67 +20,47 @@ public class DataStreamStrategy implements AbstractStrategy {
             dataOutputStream.writeUTF(resume.getUuid());
             dataOutputStream.writeUTF(resume.getFullName());
 
-            Map<ContactType, String> contacts = resume.getContacts();
-            dataOutputStream.writeInt(contacts.size());
-            for (Map.Entry<ContactType, String> contact : contacts.entrySet()) {
+            writeDataContainer(dataOutputStream, resume.getContacts().entrySet(), contact -> {
                 dataOutputStream.writeUTF(contact.getKey().name());
                 dataOutputStream.writeUTF(contact.getValue());
-            }
+            });
 
-            Map<SectionType, AbstractSection> sections = resume.getSections();
-            dataOutputStream.writeInt(sections.size());
-            for (Map.Entry<SectionType, AbstractSection> section : sections.entrySet()) {
-                dataOutputStream.writeUTF(section.getKey().name());
-                switch (section.getKey()) {
-                    case PERSONAL:
-                    case OBJECTIVE:
-                        writeTextSection(dataOutputStream, (TextSection) section.getValue());
-                        break;
-                    case ACHIEVEMENT:
-                    case QUALIFICATIONS:
-                        writeListSection(dataOutputStream, (ListSection) section.getValue());
-                        break;
-                    case EXPERIENCE:
-                    case EDUCATION:
-                        writeInstitutionSection(dataOutputStream, (InstitutionSection) section.getValue());
-                        break;
-                }
-            }
+            writeDataContainer(dataOutputStream, resume.getSections().entrySet(), entity -> {
+                        SectionType sectionType = entity.getKey();
+                        AbstractSection section = entity.getValue();
+                        dataOutputStream.writeUTF(sectionType.name());
+                        switch (sectionType) {
+                            case PERSONAL:
+                            case OBJECTIVE:
+                                dataOutputStream.writeUTF(((TextSection) section).getContent());
+                                break;
+                            case ACHIEVEMENT:
+                            case QUALIFICATIONS:
+                                writeDataContainer(dataOutputStream, ((ListSection) section).getContents(), dataOutputStream::writeUTF);
+                                break;
+                            case EXPERIENCE:
+                            case EDUCATION:
+                                writeDataContainer(dataOutputStream, ((InstitutionSection) section).getInstitutions(), institution -> {
+                                    dataOutputStream.writeUTF(institution.getHomePage().getName());
+                                    dataOutputStream.writeUTF(institution.getHomePage().getUrl());
+                                    writeDataContainer(dataOutputStream, institution.getPositions(), position -> {
+                                        dataOutputStream.writeUTF(position.getTitle());
+                                        writeLocalDate(dataOutputStream, position.getStartDate());
+                                        writeLocalDate(dataOutputStream, position.getEndDate());
+                                        dataOutputStream.writeUTF(position.getDescription());
+                                    });
+                                });
+                                break;
+                        }
+                    }
+            );
         }
     }
 
-    private void writeTextSection(DataOutputStream dataOutputStream, TextSection section) throws IOException {
-        dataOutputStream.writeUTF(section.getContent());
-    }
-
-    private void writeListSection(DataOutputStream dataOutputStream, ListSection section) throws IOException {
-        List<String> contents = section.getContents();
-        dataOutputStream.writeInt(contents.size());
-        for (String content : contents) {
-            dataOutputStream.writeUTF(content);
-        }
-    }
-
-    private void writeInstitutionSection(DataOutputStream dataOutputStream, InstitutionSection section) throws IOException {
-        List<Institution> institutions = section.getInstitutions();
-        dataOutputStream.writeInt(institutions.size());
-
-        for (Institution institution : institutions) {
-            dataOutputStream.writeUTF(institution.getHomePage().getName());
-            dataOutputStream.writeUTF(institution.getHomePage().getUrl());
-            writePositions(dataOutputStream, institution);
-        }
-    }
-
-    private void writePositions(DataOutputStream dataOutputStream, Institution institution) throws IOException {
-        List<Institution.Position> positions = institution.getPositions();
-        dataOutputStream.writeInt(positions.size());
-
-        for (Institution.Position position : positions) {
-            dataOutputStream.writeUTF(position.getTitle());
-            writeLocalDate(dataOutputStream, position.getStartDate());
-            writeLocalDate(dataOutputStream, position.getEndDate());
-            dataOutputStream.writeUTF(position.getDescription());
+    private <T> void writeDataContainer(DataOutputStream dataOutputStream, Collection<T> collection, DataStreamWriter<T> writer) throws IOException {
+        dataOutputStream.writeInt(collection.size());
+        for (T element : collection) {
+            writer.write(element);
         }
     }
 
